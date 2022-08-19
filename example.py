419b90be2a2a4317aab2df9311052a2cbc8fbd5d
ryan <ryan@fannon.net>
2022-08-19 11:51:56 -0600

    Added icon replace example

--- example.py
+++ example.py
@@ -19,6 +19,11 @@ print(broken_vms.info["crc"])
 print(broken_vms.generated_crc())
 broken_vms.fix_crc()
 
+# vms_replace icon
+old_icon = vmut.vms.load_vms("example_files/THEME001.VMS")
+old_icon.update_icon('example_files/ICONDATA.png', 'replaced_icon.vms')
+old_icon.image_save("replaced_icon.png")
+
 # VMI generate
 gamesave_vms.vmi_gen("test.vmi", "slurmking.com", "test VMI file")
 
@@ -28,9 +33,9 @@ print(VMI_read.info)
 print(VMI_read.creation_string)
 
 # ICONDATA create
-vmut.ICONDATA(image="example_files/slurm.png", threshold=160, invert=True).save("example_files/")
+vmut.Icon(image="example_files/slurm.png", threshold=160, invert=True).save("example_files/")
 # ICONDATA export generated images
-vmut.ICONDATA(image="example_files/slurm.png", threshold=160, invert=True, export=True)
+vmut.Icon(image="example_files/slurm.png", threshold=160, invert=True, export=True)
 
 # ICONDATA export
 ICONDATA_vms = vmut.vms.load_vms("example_files/ICONDATA.VMS")
